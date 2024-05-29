c357723c4bfe54261836ef802e6b21f3b34ecfd8
Deny W <115266792+denywa@users.noreply.github.com>
2024-05-30 02:17:38 +0800

    Delete src directory

--- src/main/java/com/data/siata/repository/CalendarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.data.siata.repository;
-
-import com.data.siata.model.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Integer> {
-}
