0990046eb30fce9946c442f2bb926bc48958f0b6
Deny W <115266792+denywa@users.noreply.github.com>
2024-06-13 20:59:42 +0800

    Import JsonIgnore

--- Siata Springboot/src/main/java/com/data/siata/model/User.java
+++ Siata Springboot/src/main/java/com/data/siata/model/User.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
