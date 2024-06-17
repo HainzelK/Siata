c37c5c57bdb66da8ee04941c071fa3d8473c6058
Aaron Jevon Benedict Kongdoh <148040924+trfyrt@users.noreply.github.com>
2024-06-17 23:21:39 +0800

    Delete Siata Springboot/src/main/java/com/data/siata/siata directory

--- Siata Springboot/src/main/java/com/data/siata/siata/repository/DestinationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.data.siata.repository;
-
-import com.data.siata.model.Destination;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinationRepository extends JpaRepository<Destination, Integer> {
-}
