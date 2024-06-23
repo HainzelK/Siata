b6d7eb302b170c0fb0fd1e87e452045cfa50ff05
Hainzel <hainzelk@gmail.com>
2024-06-23 17:07:58 +0800

    Siata Final

--- Siata Springboot/src/main/java/com/data/siata/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.data.siata.repository;
-
-import com.data.siata.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
